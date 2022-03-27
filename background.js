4b1bb904a0ce3a2205b8bb439cc65e67e82723f1
hms5232 <hms5232@yahoo.com.tw>
2022-03-27 21:50:13 +0800

    wip: 可以依照狀態更換 icon
    但無法變更回去
    因為 setTimeOut 是 eval()
    嚴格受限
    還要研究方法

--- background.js
+++ background.js
@@ -1,9 +1,20 @@
+const DFEAULT_ICON = 'icons/btn-96.png';
+const OK_ICON = 'icons/copy-ok-100.png';
+const ERROR_ICON = 'icons/copy-error-100.png';
+
 browser.browserAction.onClicked.addListener((tab) => {
     // decodeURI(window.location.href);
     const duri = decodeURI(tab.url);
     navigator.clipboard.writeText(duri).then(() => {
-        console.log(`${duri}`)
+        console.log(`${duri}`);
+        showStatusByIcon(true);
     }).catch(err => {
         console.error(err);
+        showStatusByIcon(false);
     })
 });
+
+function showStatusByIcon(status){
+    browser.browserAction.setIcon({path: status ? OK_ICON : ERROR_ICON});
+    setTimeout(browser.browserAction.setIcon({path: DFEAULT_ICON}), 3000);
+}
